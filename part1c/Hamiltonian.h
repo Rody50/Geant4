a1a82d2d2994eafd956709fa65ee4e8070205acd
Rodric Seutin <rodric.seutin@student.kuleuven.be>
2018-07-17 16:19:17 +0800

    Start of code.

---
+++ part1c/Hamiltonian.h
@@ -0,0 +1,10 @@
+/* Header file for exercise 1c (Hamiltonian.h) */
+
+#include "armadillo"
+#include <iostream>
+
+using namespace arma;
+using std::cout;
+
+const int n = 6;
+mat hamiltonian(n, n);
