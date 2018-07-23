90e1eaaa7d0b7fc42c980599384a7b981fc926f1
Rodric Seutin <rodric.seutin@student.kuleuven.be>
2018-07-23 17:59:28 +0800

    Constructed the Tensors class

---
+++ part1c/src/Tensor.h
@@ -0,0 +1,17 @@
+/* Header file of the tensor class */
+#ifndef TENSOR_H
+#define TENSOR_H
+
+#include <vector>
+
+using std::vector;
+
+class Tensor
+{
+	private:
+		vector<double> fTensor;
+
+	public:
+}
+
+#endif // TENSOR_H
